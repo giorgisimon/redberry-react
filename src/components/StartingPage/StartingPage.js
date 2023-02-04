e5db80aa7cc5a3f8a88b1969e460258fd84a3442
gio <giokaspi@gmail.com>
2023-02-04 19:48:16 +0400

    First Page Visuality
    just finished visualisation of first page using react components

---
+++ src/components/StartingPage/StartingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./StartingPage.css";
+
+function StartingPage() {
+  return (
+    <div>
+      <span className="logo"></span>
+      <span className="line"></span>
+      <span className="mark"></span>
+      <button>რეზიუმეს დამატება</button>
+    </div>
+  );
+}
+
+export default StartingPage;
